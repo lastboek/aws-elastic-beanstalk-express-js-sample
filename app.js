ce36ec4f479a0194b5530124aa5c950240a2e409
lastboek <lastboek@gmail.com>
2021-09-14 13:17:07 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Committing this to github!'));
+app.get('/', (req, res) => res.send('I\'m adding a manual review!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
